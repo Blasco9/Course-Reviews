d106e3034414848682734e5f9b6a8eb75e96e4a3
Julian Blasco <blascoblasco9@gmail.com>
2020-06-28 22:57:07 -0300

    Create courses controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'reviews#index'
   resources :users, except: :index
   resources :reviews
+  resources :courses, except: :destroy
   resources :followings, only: [:create, :destroy]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
