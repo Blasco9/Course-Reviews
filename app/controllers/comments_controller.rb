cf8807b9da1d5543f6c02bda269a82cc6825b1dc
Julian Blasco <blascoblasco9@gmail.com>
2020-06-29 17:24:54 -0300

    Create comments controller, view and styles

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to @comment.review, notice: 'You commented on the review'
+    else
+      redirect_to root_path, alert: @comment.errors.full_messages
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @comment.review, notice: 'Comment deleted successfuly'
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:text, :review_id)
+    end
+end
